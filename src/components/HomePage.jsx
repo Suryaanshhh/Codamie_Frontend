08d7799ad8e11d04d2cf7e1628dfae5d9ab88180
Suryaanshhh <suryanshdwivedi615@gmail.com>
2025-03-17 22:31:36 +0530

    Frontend initiated and HomePage created

---
+++ src/components/HomePage.jsx
@@ -0,0 +1,29 @@
+export const HomePage = () => {
+    return <div className="h-screen bg-[#FFB6C1] ">
+        <div className="flex justify-between">
+            <div>
+                <div className="text-4xl font-bold text-gray-600">
+                    CODAMIE
+                </div>
+                <div className="font-semibold text-gray-600">
+                    Find Your Coding Partner
+                </div>
+            </div>
+
+            <div>
+                <button className="font-semibold text-gray-600 m-2 cursor-pointer">Home</button>
+                <button className="font-semibold text-gray-600 m-2 cursor-pointer">About</button>
+                <button className="font-semibold text-gray-600 m-2 cursor-pointer">Contact Us</button>
+            </div>
+
+            <div className >
+                <button className="font-semibold text-gray-600 m-2 cursor-pointer">Login</button>
+                <button className="font-semibold text-gray-600 m-2 cursor-pointer">Signup</button>
+            </div>
+        </div>
+
+     
+
+    </div>
+
+}
